39fb8d31e4d563c57f607a896a8451b3ef40e43a
danilo torres lima <danilotl21@gmail.com>
2019-12-12 19:52:43 -0300

    segunda questão completa

---
+++ ex02/js/script.js
@@ -0,0 +1,8 @@
+let num=0;
+let maior=0;
+for(var i = 0; i < 5; i++){
+    num = prompt('digite um numero:' );
+        if(num > maior){
+            maior = num;  }
+}
+alert('Maior número: '+ maior); 
