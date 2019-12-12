7ba3113c847c227bb2b87bf87b0ba1504bb61720
danilojtl <58794136+danilojtl@users.noreply.github.com>
2019-12-11 22:00:53 -0300

    resposta questão 1

---
+++ pasta/js/script.js
@@ -0,0 +1,12 @@
+let  notas=0;
+let  soma=0;
+var  i=0; 
+while(notas != 'fim'){
+	notas = prompt('Qual sua nota?'."" );
+	if(notas == 'fim')  { 
+		notas += number(n);
+		i++
+	}
+		let media=notas/i;
+		alert('notas' media);
+}
